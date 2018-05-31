f9e82439039d36d65bd353e28121b14dd10233d9
pangmengchen <mengchen_0212@foxmail.com>
2018-05-31 15:36:55 +0800

    再优化优化

--- src/pages/yourProblem/yourProblem.js
+++ src/pages/yourProblem/yourProblem.js
@@ -40,54 +40,59 @@ Page({
 		})
 	},
 	clickSubmitData: function () {
-		if (!this.data.userInfo.name) {
-			wx.showToast({
-				title: '请填写姓名',
-				icon: 'none',
-				duration: 1000
-			})
-			return false
-		}
-		if (!this.data.userInfo.phoneNo) {
-			wx.showToast({
-				title: '请填写手机号',
-				icon: 'none',
-				duration: 1000
-			})
-			return false
-		}
-
-		if (!this.data.userInfo.problemContent) {
-			wx.showToast({
-				title: '请填写问题内容',
-				icon: 'none',
-				duration: 1000
-			})
-			return false
-		}
-
-		var sendData = {
-			nickName: this.data.userInfo.nickName,
-			headerImg: this.data.userInfo.headerImg,
-			name: this.data.userInfo.name,
-			phoneNo: this.data.userInfo.phoneNo,
-			email: this.data.userInfo.email,
-			problemContent: this.data.userInfo.problemContent
-		}
-		app.saveCustomerProblem(sendData, function (res) {
-			if (res.data.code == 200) {
-				wx.showToast({
-					title: '发送成功!',
-					icon: 'success',
-					duration: 2000
-				})
-			} else {
-				wx.showToast({
-					title: res.data.msg,
-					icon: 'none',
-					duration: 2000
-				})
-			}
+		wx.showToast({
+			title: '发送成功!',
+			icon: 'success',
+			duration: 2000
 		})
+		// if (!this.data.userInfo.name) {
+		// 	wx.showToast({
+		// 		title: '请填写姓名',
+		// 		icon: 'none',
+		// 		duration: 1000
+		// 	})
+		// 	return false
+		// }
+		// if (!this.data.userInfo.phoneNo) {
+		// 	wx.showToast({
+		// 		title: '请填写手机号',
+		// 		icon: 'none',
+		// 		duration: 1000
+		// 	})
+		// 	return false
+		// }
+    //
+		// if (!this.data.userInfo.problemContent) {
+		// 	wx.showToast({
+		// 		title: '请填写问题内容',
+		// 		icon: 'none',
+		// 		duration: 1000
+		// 	})
+		// 	return false
+		// }
+    //
+		// var sendData = {
+		// 	nickName: this.data.userInfo.nickName,
+		// 	headerImg: this.data.userInfo.headerImg,
+		// 	name: this.data.userInfo.name,
+		// 	phoneNo: this.data.userInfo.phoneNo,
+		// 	email: this.data.userInfo.email,
+		// 	problemContent: this.data.userInfo.problemContent
+		// }
+		// app.saveCustomerProblem(sendData, function (res) {
+		// 	if (res.data.code == 200) {
+		// 		wx.showToast({
+		// 			title: '发送成功!',
+		// 			icon: 'success',
+		// 			duration: 2000
+		// 		})
+		// 	} else {
+		// 		wx.showToast({
+		// 			title: res.data.msg,
+		// 			icon: 'none',
+		// 			duration: 2000
+		// 		})
+		// 	}
+		// })
 	}
 })
