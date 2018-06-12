53db7f04eaae98722c9cb65b89ec41069a4fb392
pangmengchen <mengchen_0212@foxmail.com>
2018-06-12 21:03:12 +0800

    创客宝开始

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,12 +2,35 @@ var app = getApp()
 Page({
 	data: {
 		menuArr: [
-			{name: '关于我们', link: '/pages/aboutUs/aboutUs'},
-			{name: '业务范围', link: '/pages/business/business'},
-			{name: '您的问题', link: '/pages/yourProblem/yourProblem'},
-			{name: '真诚服务', link: '/pages/myService/myService'},
-			{name: '联系我们', link: '/pages/contactUs/contactUs'}
-		]
+			{name: '公司动态', link: '/pages/company-news/company-news'},
+			{name: '新闻详情', link: '/pages/new-detail/new-detail'},
+			{name: '成功案例', link: '/pages/example/example'},
+			{name: '案例详情', link: '/pages/example-detail/example-detail'},
+			{name: '联系我们', link: '/pages/contact-us/contact-us'},
+			{name: '关于我们', link: '/pages/about-us/about-us'}
+		],
+		indicatorDots: true,
+		autoplay: true,
+		interval: 3000,
+		duration: 1000,
+		loadingHidden: false, // loading
+		userInfo: {},
+		swiperCurrent: 0,
+		selectCurrent: 0,
+		categories: [],
+		activeCategoryId: 0,
+		goods: [],
+		scrollTop: '0',
+		loadingMoreHidden: true,
+
+		hasNoCoupons: true,
+		coupons: [],
+		searchInput: '',
+		banners: [
+			{picUrl: '../../image/ckb/banner1.jpg', businessId: '1'},
+			{picUrl: '../../image/ckb/banner2.jpg', businessId: '2'},
+			{picUrl: '../../image/ckb/banner3.jpg', businessId: '3'}]
+
 	},
 	onLoad: function () {
 
