1d6e2c9aeddd22bfb49c2b8f7ca88fcbe2e3f081
FizzPang <mengchen_0212@foxmail.com>
2018-06-16 22:48:02 +0800

    genju kehu yaoqiu xiugai

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -20,16 +20,17 @@ Page({
 			{picUrl: 'http://i4.bvimg.com/649796/92583979b8f78553.png', businessId: '3'},
 			{picUrl: 'http://i4.bvimg.com/649796/25b01da2f746ea35.png', businessId: '3'},
 			{picUrl: 'http://i4.bvimg.com/649796/ab4099d65e0c9640.png', businessId: '3'}],
-		newsArr: [{title: '创客宝云铺商城新增蜂鸟订单专属平台红包', logo: 'http://i2.bvimg.com/649796/529498f8ac94a9ea.png', id: '1'},
-			{title: '祝所有女神节日快乐', logo: 'http://i2.bvimg.com/649796/6f027d1cb7f45548.png', id: '2'},
-			{title: '创客宝入驻商家突飞猛进', logo: 'http://i2.bvimg.com/649796/f8b21c027df0a0c8.png', id: '3'},
+		newsArr: [{title: '创客宝入驻商家突飞猛进', logo: 'http://i2.bvimg.com/649796/f8b21c027df0a0c8.png', id: '3'},
+			{title: '创客宝云铺商城新增蜂鸟订单专属平台红包', logo: 'http://i2.bvimg.com/649796/529498f8ac94a9ea.png', id: '1'},
 			{title: '实体商家免费加入创客宝,轻松实现月入过万', logo: 'http://i2.bvimg.com/649796/a89c1236a325059d.png', id: '4'},
-			{title: '创客宝回馈客户,随机立减百元大奖等你拿', logo: 'http://i2.bvimg.com/649796/c9e44468856d94b3.png', id: '5'}],
-		picArr: ['http://i4.bvimg.com/649796/a3e3c98e90626845.png',
-			'http://i4.bvimg.com/649796/c561466972bffefe.png',
-			'http://i4.bvimg.com/649796/92583979b8f78553.png',
-			'http://i4.bvimg.com/649796/25b01da2f746ea35.png',
-			'http://i4.bvimg.com/649796/ab4099d65e0c9640.png']
+			{title: '创客宝回馈客户,随机立减百元大奖等你拿', logo: 'http://i2.bvimg.com/649796/c9e44468856d94b3.png', id: '5'},
+			{title: '祝所有女神节日快乐', logo: 'http://i2.bvimg.com/649796/6f027d1cb7f45548.png', id: '2'},],
+		picArr: [
+			'http://i4.bvimg.com/649796/a3e3c98e90626845.png',
+			'http://i2.bvimg.com/649796/68f24db09270379b.jpg',
+			'http://i2.bvimg.com/649796/f46575250feda4eb.jpg',
+			'http://i2.bvimg.com/649796/57e53cdedb44aa35.jpg',
+			'http://i2.bvimg.com/649796/12911053e53753b0.jpg']
 
 	},
 	onLoad: function () { },
