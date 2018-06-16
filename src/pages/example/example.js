f7d7c1dd8d451b806e136731359653a296e7f085
FizzPang <mengchen_0212@foxmail.com>
2018-06-16 20:56:30 +0800

    出版

--- src/pages/example/example.js
+++ src/pages/example/example.js
@@ -3,8 +3,17 @@
 var app = getApp()
 Page({
 	data: {
-		newsArr: [{}, {}, {}, {}, {}],
+		newsArr: [
+			{logo: 'http://i2.bvimg.com/649796/42b8382fb5e8211a.png', title: '支付收益一多码付', id: 101},
+			{logo: 'http://i2.bvimg.com/649796/0aa922b25a24e1ad.png', title: '大数据中心,轻松锁客', id: 102},
+			{logo: 'http://i2.bvimg.com/649796/b55002be74b794ca.png', title: '创客宝,让零售更智慧', id: 103},
+			{logo: 'http://i2.bvimg.com/649796/45028c9a8830156c.png', title: '决战新零售的四要素', id: 104},
+			{logo: 'http://i2.bvimg.com/649796/10e02b8dd126cfb3.png', title: '创客宝多码付全线启动', id: 105}
+		],
 	},
-	onLoad: function () {}
+	onLoad: function () {},
+	goHref: function (even) {
+		wx.navigateTo({url: '/pages/new-detail/new-detail?id=' + even.currentTarget.dataset.id})
+	}
 
 })
--- src/pages/new-detail/new-detail.js
+++ src/pages/new-detail/new-detail.js
@@ -6,7 +6,13 @@ Page({
 			2: {title: '祝所有女神节日快乐', logo: 'http://i2.bvimg.com/649796/e11401a0e3ab989b.jpg', id: '2'},
 			3: {title: '创客宝入驻商家突飞猛进', logo: 'http://i2.bvimg.com/649796/792c5ad474c49308.jpg', id: '3'},
 			4: {title: '实体商家免费加入创客宝,轻松实现月入过万', logo: 'http://i2.bvimg.com/649796/3618fa9647fe512e.jpg', id: '4'},
-			5: {title: '创客宝回馈客户,随机立减百元大奖等你拿', logo: 'http://i2.bvimg.com/649796/a8ca6bc1fb01c84f.jpg', id: '5'}
+			5: {title: '创客宝回馈客户,随机立减百元大奖等你拿', logo: 'http://i2.bvimg.com/649796/a8ca6bc1fb01c84f.jpg', id: '5'},
+
+			101: {title: '支付收益一多码付', logo: 'http://i2.bvimg.com/649796/48bb7e7f62cd6cc7.jpg', id: '101'},
+			102: {title: '大数据中心,轻松锁客', logo: 'http://i2.bvimg.com/649796/9b2321b38534a662.jpg', id: '102'},
+			103: {title: '创客宝,让零售更智慧', logo: 'http://i2.bvimg.com/649796/f228df888f30efa3.jpg', id: '103'},
+			104: {title: '决战新零售的四要素', logo: 'http://i2.bvimg.com/649796/25eb46043ea06863.jpg', id: '104'},
+			105: {title: '创客宝多码付全线启动', logo: 'http://i2.bvimg.com/649796/72a4d659fde343c4.jpg', id: '105'}
 		},
 		newsImg: '',
 		title: ''
