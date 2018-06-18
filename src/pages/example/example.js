956b2a5db8e988ae0ec247e7df6cd5ad91ebe809
FizzPang <mengchen_0212@foxmail.com>
2018-06-18 19:07:04 +0800

    页面优化

--- src/pages/example/example.js
+++ src/pages/example/example.js
@@ -8,7 +8,8 @@ Page({
 			{logo: 'http://i2.bvimg.com/649796/0aa922b25a24e1ad.png', title: '大数据中心,轻松锁客', id: 102},
 			{logo: 'http://i2.bvimg.com/649796/b55002be74b794ca.png', title: '创客宝,让零售更智慧', id: 103},
 			{logo: 'http://i2.bvimg.com/649796/45028c9a8830156c.png', title: '决战新零售的四要素', id: 104},
-			{logo: 'http://i2.bvimg.com/649796/10e02b8dd126cfb3.png', title: '创客宝多码付全线启动', id: 105}
+			{logo: 'http://i2.bvimg.com/649796/10e02b8dd126cfb3.png', title: '创客宝多码付全线启动', id: 105},
+			{logo: 'http://i4.bvimg.com/649796/95aef107c08d44ea.png', title: '创客宝扫码支付介绍', id: 106},
 		],
 	},
 	onLoad: function () {},
