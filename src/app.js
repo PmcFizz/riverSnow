f99e14754f2ed75a9ad641209a91a5e205ff1bdb
pangmengchen <mengchen_0212@foxmail.com>
2018-06-19 09:44:29 +0800

    接口配置

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
 //app.js
 App({
-	serverHost: 'https://www.iwin8.cc', // 服务器Host
-	onLaunch: function () { },
+	serverHost: 'https://www.easy-mock.com/mock/5b2857a7397321317275c87b/api', // 服务器Host
+	onLaunch: function () {},
 	// 全局数据
 	globalData: {
 		userInfo: null,
 		sessionId: ''
 	},
-	// 联系我们
-	contentUs: function (data, cb) {
-		var sessionId = this.globalData.sessionId
-		wx.request({
-			url: this.serverHost + '/module/leaveWord/save.do',
+	// 获取首页banner
+	getBanner: function (data, cb) {
+		wx.reqest({
+			url: this.serverHost + '/getBanner',
 			data: data,
-			header: {'Cookie': 'JSESSIONID=' + sessionId},
-			method: 'POST',
+			method: 'GET',
 			success: function (res) {
 				if (res.statusCode === 200) {
 					typeof cb == 'function' && cb(res)
@@ -22,33 +20,38 @@ App({
 			}
 		})
 	},
-	// 获取当前用户信息
-	getUserInfo: function (cb) {
-		var that = this
-		if (this.globalData.userInfo) {
-			typeof cb == 'function' && cb(this.globalData.userInfo)
-		} else {
-			//调用登陆接口
-			wx.login({
-				success: function (loginRes) {
-					// 用户登录后获取code 回传开发服务器 获取openid
-					console.log(loginRes)
-					wx.getUserInfo({
-						success: function (res) {
-							that.globalData.userInfo = res.userInfo
-							typeof cb == 'function' && cb(that.globalData.userInfo)
-						}
-					})
-				}
-			})
-		}
-	},
-	// 保存客户问题
-	saveCustomerProblem: function (data, cb) {
-		wx.request({
-			url: this.serverHost + '/module/leaveWord/save.do',
+	// 获取案例列表
+	getExampleList: function (data, cb) {
+		wx.reqest({
+			url: this.serverHost + '/getExampleList',
 			data: data,
-			method: 'POST',
+			method: 'GET',
+			success: function (res) {
+				if (res.statusCode === 200) {
+					typeof cb == 'function' && cb(res)
+				}
+			}
+		})
+	},
+	// 获取新闻列表
+	getNewsList: function (data, cb) {
+		wx.reqest({
+			url: this.serverHost + '/getNewsList',
+			data: data,
+			method: 'GET',
+			success: function (res) {
+				if (res.statusCode === 200) {
+					typeof cb == 'function' && cb(res)
+				}
+			}
+		})
+	},
+	// 获取公司信息
+	getCompanyInfo: function (data, cb) {
+		wx.reqest({
+			url: this.serverHost + '/getCompanyInfo',
+			data: data,
+			method: 'GET',
 			success: function (res) {
 				if (res.statusCode === 200) {
 					typeof cb == 'function' && cb(res)
