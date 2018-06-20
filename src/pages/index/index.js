a7244921500011a5534bf9a9f666d93007a1bfae
pangmengchen <mengchen_0212@foxmail.com>
2018-06-20 19:25:14 +0800

    最后一版吧

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -12,7 +12,8 @@ Page({
 			{picUrl: 'http://i4.bvimg.com/649796/92583979b8f78553.png', businessId: '3'},
 			{picUrl: 'http://i4.bvimg.com/649796/25b01da2f746ea35.png', businessId: '3'},
 			{picUrl: 'http://i4.bvimg.com/649796/ab4099d65e0c9640.png', businessId: '3'}],
-		newsArr: [{title: '创客宝入驻商家突飞猛进', logo: 'http://i2.bvimg.com/649796/f8b21c027df0a0c8.png', id: '3'},
+		newsArr: [
+			{title: '创客宝入驻商家突飞猛进', logo: 'http://i2.bvimg.com/649796/f8b21c027df0a0c8.png', id: '3'},
 			{title: '创客宝云铺商城新增蜂鸟订单专属平台红包', logo: 'http://i2.bvimg.com/649796/529498f8ac94a9ea.png', id: '1'},
 			{title: '实体商家免费加入创客宝,轻松实现月入过万', logo: 'http://i2.bvimg.com/649796/a89c1236a325059d.png', id: '4'},
 			{title: '创客宝回馈客户,随机立减百元大奖等你拿', logo: 'http://i2.bvimg.com/649796/c9e44468856d94b3.png', id: '5'},
@@ -26,7 +27,7 @@ Page({
 
 	},
 	onLoad: function () {
-		// this.getBanner()
+		this.setData({newsArr: app.globalData.newsList})
 	},
 	swiperchange: function (e) {
 		this.setData({swiperCurrent: e.detail.current})
