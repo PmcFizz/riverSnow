c5e3f608b3e910f70a30fde95d6a77d61d510c51
FizzPang <mengchen_0212@foxmail.com>
2018-08-02 22:08:03 +0800

    优化小程序

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ App({
 			{
 				logo: 'http://mczaiyun.top/ckb/21.jpg',
 				title: '关注天美汇鸡翅包飯明星网红小吃',
+				color: 'red',
 				id: '1',
 				content: ['http://mczaiyun.top/tmh/e9.png', 'http://mczaiyun.top/tmh/e10.png', 'http://mczaiyun.top/tmh/e11.png', 'http://mczaiyun.top/tmh/e12.png', 'http://mczaiyun.top/tmh/e13.png']
 			},
@@ -17,18 +18,21 @@ App({
 				title: '天美汇鸡翅包饭配特调茶饮小资生活',
 				logo: 'http://mczaiyun.top/ckb/1.png',
 				id: '2',
+				color: '#fdd34d',
 				content: ['http://mczaiyun.top/tmh/e14.png', 'http://mczaiyun.top/tmh/e15.png', 'http://mczaiyun.top/tmh/e16.png']
 			},
 			{
 				title: '网红美食天美汇鸡翅包饭十大加盟优势',
 				logo: 'http://mczaiyun.top/ckb/3.png',
 				id: '3',
+				color: 'blue',
 				content: ['http://mczaiyun.top/tmh/e17.png', 'http://mczaiyun.top/tmh/e18.png', 'http://mczaiyun.top/tmh/e19.png']
 			},
 			{
 				title: '天美汇鸡翅包饭合作加盟类型',
 				logo: 'http://mczaiyun.top/ckb/5.png',
 				id: '4',
+				color: '#f6ba40',
 				content: ['http://mczaiyun.top/tmh/e20.png', 'http://mczaiyun.top/tmh/e21.png', 'http://mczaiyun.top/tmh/e22.png', 'http://mczaiyun.top/tmh/e23.png',
 					'http://mczaiyun.top/tmh/e24.png', 'http://mczaiyun.top/tmh/e25.png', 'http://mczaiyun.top/tmh/e26.png']
 			},
@@ -36,6 +40,7 @@ App({
 				title: '加盟天美汇鸡翅包饭投资预算',
 				logo: 'http://mczaiyun.top/ckb/7.png',
 				id: '5',
+				color: 'red',
 				content: ['http://mczaiyun.top/tmh/e27.png', 'http://mczaiyun.top/tmh/e28.png', 'http://mczaiyun.top/tmh/e29.png']
 			}
 		],
